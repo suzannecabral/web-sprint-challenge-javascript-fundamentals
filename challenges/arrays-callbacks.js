d6cdc9884bb27b47e29f2e5e0fa8914cff74e5c6
Suzanne Cabral <suzannecabral@live.com>
2020-08-21 10:34:31 -0700

    Stretch complete, array methods all have arrow functions

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -117,7 +117,7 @@ console.log(consume(10, 16, multiply)); // 160
 console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-console.log("[ ] ------ Stretch: Arrays & Callbacks ------ ")
+console.log("[x] ------ Stretch: Arrays & Callbacks ------ ")
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
