9e5e7602cd1c3d6a0e6ac4f572bd7feff51f2fe8
Suzanne Cabral <suzannecabral@live.com>
2020-08-21 08:50:16 -0700

    arrays only complete, working on callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -78,6 +78,10 @@ console.log(populationTotal);
 */
 
 
+
+
+
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
